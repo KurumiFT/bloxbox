d8277fed9d6b808ac72df106302ad9c059c7a52d
OntoTube <35110747+OntoTube@users.noreply.github.com>
2022-11-04 21:45:50 +0300

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 var pg = require('pg');
-var conString = "postgres://admin:aG4qbi7ealJJIYqLOZsNnBX6RMOgsf8b@dpg-cdihrtqen0hi0coa5vb0-a.frankfurt-postgres.render.com/items?ssl=true";
+var conString = "postgres://admin:aG4qbi7ealJJIYqLOZsNnBX6RMOgsf8b@dpg-cdihrtqen0hi0coa5vb0-a/items";
 
 var client = new pg.Client(conString);
 client.connect();
@@ -19,4 +19,4 @@ app.get('/item', (request, response) => {
 });
 
 client.query("SELECT * FROM prototype WHERE item_id=1").then(result => {console.log(result['rows'][0]['display_name'])});
-app.listen(3000);
+app.listen(process.env.PORT || 3001);
