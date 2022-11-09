e3d4c0cb7e7a09df9915cc59546c3148bb2c13a8
OntoTube <35110747+OntoTube@users.noreply.github.com>
2022-11-09 20:40:20 +0300

    Update app.js

--- app.js
+++ app.js
@@ -10,13 +10,11 @@ client.connect();
 app.get('/item', (request, response) => {
 	client.query("SELECT * FROM prototype WHERE item_id="+request.query.id).then(result => {
 		let row = result['rows'][0];
-		let data = {"display_name": row['display_name'], "weight": row["weight"], "image_href": row["image_href"]};
+		let data = {"display_name": row['display_name'], "weight": row["weight"], "image_href": row["image_href"], "hold_in_arm": row["holdinarm"], "item_type": row["item_type"]};
 
 		response.send(JSON.stringify(data));
 	});
 
 	let data = {};
 });
-
-client.query("SELECT * FROM prototype WHERE item_id=1").then(result => {console.log(result['rows'][0]['display_name'])});
 app.listen(process.env.PORT || 3001);
